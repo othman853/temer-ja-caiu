757ececcab64af1902f5c2f82ea127afd21dc399
othman853 <anuar.yasser@gmail.com>
2017-05-17 23:45:03 -0300

    Aguardando o momento de tchau, querido.

---
+++ index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+// app.user(express.static(path.join(__dirname, 'static')));
+
+app.get('/', (_, res) => res.sendFile(path.join(__dirname, 'static', 'index.html')));
+
+app.listen(port, console.log.bind(console, `up on ${port}`));
